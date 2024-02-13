7175a9f87800f908c0fa920be2de4df1200635e1
LAYXSAMZ <kevin53846.914@gmail.com>
2024-02-14 04:34:23 +0530

    login toast condition change

--- app/src/main/java/com/example/sweet_wave/Login.java
+++ app/src/main/java/com/example/sweet_wave/Login.java
@@ -5,21 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 Button login;
+    View v;
 TextView fpwd,crtacc;
 EditText usr,pwd;
+    Handler h=new Handler();
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
 
         login=findViewById(R.id.login);
         usr=findViewById(R.id.usr);
@@ -52,9 +62,20 @@ EditText usr,pwd;
         crtacc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                h.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                crtacc.setText("Sign up");
+                    }
+                }, 1000);
+                crtacc.setText(Html.fromHtml("<u>Sign up</u> "));
+
                 startActivity(new Intent(getApplicationContext(), sign_up.class));
+
             }
         });
+
 
 
 
